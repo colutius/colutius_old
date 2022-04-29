e973b5eee14df2811dd187acb37ffcffcb6f23a8
ambition_echo <ambition_echo@outlook.com>
2022-04-29 21:25:49 +0800

    修复频道列表清空不干净的bug,添加回车发送消息

--- src/ui/mainwidget.cc
+++ src/ui/mainwidget.cc
@@ -59,11 +59,20 @@ void MainWidget::setStyle()
 //初始化信号槽
 void MainWidget::initConnect()
 {
+    // 打开登录窗口
     connect(ui->addServerBtn, &QPushButton::clicked, this, &MainWidget::login);
+    // 发送消息
     connect(ui->sendBtn, &QPushButton::clicked, this, &MainWidget::sendMsg);
+    // 打开设置窗口
     connect(ui->settingBtn, &QPushButton::clicked, this, &MainWidget::setting);
+    // 添加频道
     connect(ui->addChannelBtn, &QPushButton::clicked, this, &MainWidget::addChannel);
+    // 刷新频道列表
     connect(ui->serverList, &QListWidget::currentItemChanged, this, &MainWidget::refreshChannelList);
+    // 回车发送消息
+    connect(ui->msgEdit, &QLineEdit::editingFinished, this, &MainWidget::sendMsg);
+    // 回车添加频道
+    connect(ui->channelEdit, &QLineEdit::editingFinished, this, &MainWidget::addChannel);
 }
 
 //添加服务器
@@ -81,21 +90,27 @@ void MainWidget::addServer()
     this->newServer = nullptr;
 }
 
-//发送消息
+// 发送消息
 void MainWidget::sendMsg()
 {
-    //未添加服务器忽略
+    // 未添加服务器忽略
     if (this->serverList.isEmpty())
     {
         return;
     }
-
+    // 输入框为空忽略
+    QString msg = ui->msgEdit->text();
+    if (msg.isEmpty())
+    {
+        return;
+    }
+    ui->msgEdit->clear();
     // 判断当前所在服务器及频道
     int currentServerIndex = ui->serverList->currentRow();
     int currentChannelIndex = ui->channelList->currentRow();
-    //获取输入框内容
-    QString msg = "PRIVMSG " + this->serverList.at(currentServerIndex)->channelList.at(currentChannelIndex)->text() +
-                  " :" + ui->msgEdit->text();
+    // 构造发送消息
+    msg =
+        "PRIVMSG " + this->serverList.at(currentServerIndex)->channelList.at(currentChannelIndex)->text() + " :" + msg;
     // 发送消息
     if (serverList.at(currentServerIndex)->sendMsg(msg))
     {
@@ -117,7 +132,7 @@ void MainWidget::receiveMsg()
             continue;
         }
         //接收消息，isReadyRead复位
-        QStringList buf = QString(server->socket->tcpSocket->readAll()).split("\n");
+        QStringList buf = QString(server->socket->tcpSocket->readAll()).split("\r\n");
         server->isReadyRead = false;
         //分别遍历处理每一行消息
         foreach (QString i, buf)
@@ -147,13 +162,7 @@ void MainWidget::receiveMsg()
             //服务器状态信息
             else
             {
-                QFont msgFont;
-                msgFont.setPointSize(20);
-                auto *temp = new QListWidgetItem;
-                temp->setFont(msgFont);
-                temp->setText(i);
-                temp->setTextAlignment(1);
-                ui->msgList->addItem(temp);
+                qDebug() << i;
             }
         }
     }
@@ -195,6 +204,7 @@ void MainWidget::addChannel()
     {
         return;
     }
+    ui->channelEdit->clear();
     //获取当前选中服务器
     int serverIndex = ui->serverList->currentRow();
     //发送JOIN信号
@@ -212,16 +222,19 @@ void MainWidget::refreshChannelList()
 {
     //清空频道列表
     //不能用ui.channelList->clear();clear会释放所有Item的内存，之后就用不了了
-    for (int i = 0; i < ui->channelList->count(); i++)
+    //这里直接按数量来删除的话总会剩一个，实测+2之后可以删干净了
+    for (int i = 0; i < ui->channelList->count() + 2; ++i)
     {
-        ui->channelList->takeItem(i);
+        QListWidgetItem *item = ui->channelList->takeItem(0);
     }
+
     // ui->channelList->clear();
     //  判断当前所在服务器
     int currentServerIndex = ui->serverList->currentRow();
     //添加当前选中服务器加入的频道到列表显示
     foreach (QListWidgetItem *channel, this->serverList.at(currentServerIndex)->channelList)
     {
+        qDebug() << "+1";
         ui->channelList->addItem(channel);
     }
 }
